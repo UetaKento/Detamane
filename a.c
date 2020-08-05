805f6faca05b2cab746263198487e33a54cf4711
Kento Ueta <k.ueta@KentonoMacBook-Pro.local>
2020-08-06 00:35:13 +0900

    HoT:Initial revision

---
+++ a.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+int main(){
+  int num;
+  int a=0;
+  int b=0;
+  srand((unsigned)time(NULL));
+  printf("Tossing a coin...\n");
+  for(int i=1;i<4;i++){
+    num=rand()%2;
+    if(num==0){
+      printf("Round %d: Heads\n",i);
+      a++;
+    }else{
+      printf("Round %d: Tails\n",i);
+      b++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",a,b);
+}
