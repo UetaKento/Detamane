6063d417f8e2299f50027d64cb2f1026c3490a4d
UetaKento <ueken110@icloud.com>
2020-08-06 00:59:46 +0900

    User name added

--- a.c
+++ a.c
@@ -3,20 +3,10 @@
 #include <time.h>
 
 int main(){
-  int num;
-  int a=0;
-  int b=0;
-  srand((unsigned)time(NULL));
-  printf("Tossing a coin...\n");
-  for(int i=1;i<4;i++){
-    num=rand()%2;
-    if(num==0){
-      printf("Round %d: Heads\n",i);
-      a++;
-    }else{
-      printf("Round %d: Tails\n",i);
-      b++;
-    }
-  }
-  printf("Heads: %d, Tails: %d\n",a,b);
+  char name[15];
+  printf("Who are you?\n");
+  printf("> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
 }
+
